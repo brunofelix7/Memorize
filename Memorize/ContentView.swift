c148371b714245cff9a86283c6f143a87c9310ce
Bruno Felix <bruno.felix10@hotmail.com>
2020-07-04 14:41:08 -0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Bruno Felix on 04/07/20.
+//  Copyright © 2020 CS193p Student. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
